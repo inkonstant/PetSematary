ac05089a231d690f80e51b93958d45ba373cc53f
Yiannis Konstantakis <inkonstantkis@gmail.com>
2026-01-15 16:07:07 +0200

    added port number in createpool

--- backend/db.js
+++ backend/db.js
@@ -7,6 +7,7 @@ const mysql = require('mysql2/promise');
 // wrapper out of the box, which simplifies async/await usage:contentReference[oaicite:1]{index=1}.
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
